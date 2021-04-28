345346a6d1d17834e9c2e095a3dd11deb8dc1464
nmorshed <mniajx@gmail.com>
2021-04-28 17:44:03 +0600

    Plugin initialization

---
+++ coming-son.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Coming soon Page
+Plugin URI: http://betterwizard.com/
+Description: You can set a page as a coming soon page and you can build this page as you want with your editor or page builder.
+Author: Better Wizard
+Author URI: http://betterwizard.com/
+Version: 1.0.0
+Developer: Niaj Morshed
+WC tested up to: 5.2
+*/
+?>
+
+
