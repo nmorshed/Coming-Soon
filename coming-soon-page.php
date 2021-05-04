19c8fd422dabc49a41cb570a918ae049aa110298
nmorshed <mniajx@gmail.com>
2021-05-04 21:04:28 +0600

    Plugin url updated.

--- coming-soon-page.php
+++ coming-soon-page.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Coming soon Page
-Plugin URI: https://github.com/nmorshed/Coming-Soon
+Plugin URI: https://github.com/nmorshed/Coming-Soon-Page
 Description: You can set an existing page as a coming soon/Maintenance page and you can build this page as you want with your editor or favorite page builder.
 Author: Better Wizard
 Author URI: http://betterwizard.com/
